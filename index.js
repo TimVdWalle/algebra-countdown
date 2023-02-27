2db334eba02bebb423f3fba7db9345f235d36568
timvandewalle <tim@vandewalle.mobi>
2023-02-27 13:21:59 +0100

    Update to working version

--- index.js
+++ index.js
@@ -21,19 +21,31 @@ const { simplify, parse, derivative, evaluate, expression, exp} = require('mathj
 
 const operators = [
     '+',
-    '-'
+    '-',
+    '*',
+    '/',
+    // '(',
+    // ')',
+    '^',
 ];
+
+const startingNumber = 10;
+const resultMinimum = 2020;
+const resultMaximum = 2030;
 
 
 /************************************************************************************************
  *  functions
  ************************************************************************************************/
 function buildExpressions(){
-    let expression = 'start';
-    let expressions = addPart(expression, 4);
+    let expression = '';
+    let expressions = addPart(expression, startingNumber);
     return expressions;
 }
 
+// recursive function to build all possible combinations of operators and numbers 10 to 1
+// end condition = when number is 1
+// from there, we build the results by adding the previous recursive results to the current expression, and passing those results one level up
 function addPart(expression, number){
     let expressions = []
 
@@ -43,34 +55,46 @@ function addPart(expression, number){
 
     let i = 0;
     while (i < operators.length) {
-        operator = operators[i];
+        let operator = operators[i];
         i++;
-
 
         let results = addPart(expression, number - 1)
 
         let j = 0;
         while(j < results.length){
-            console.log('result = ', results[j])
-            expressions.push(number + ' '  +  operator + ' ' + results[j])
+            let newExpression = operator + ' ' + results[j];
+            expressions.push(number + ''  +  operator + '' + results[j])
             j++
         }
     }
 
-
     return expressions;
+}
+
+function evaluateExpressions(expressions){
+    let i = 0;
+    while (i < expressions.length) {
+        let expression = expressions[i]
+        let evaluated = tryParse(expression)
+
+        if(evaluated >= resultMinimum && evaluated <= resultMaximum && Number.isInteger(evaluated)){
+            console.log(expression, ' = ', evaluated)
+        }
+
+
+        i++
+    }
 }
 
 function tryParse(expression){
     try {
-        let solution = evaluate(ex);
+        let solution = evaluate(expression);
+        return solution
 
     } catch(e){
         // ignore
-        solution = -1;
+        return -1;
     }
-
-    return solution;
 }
 
 
@@ -79,5 +103,13 @@ function tryParse(expression){
  ************************************************************************************************/
 console.log("starting app");
 
+// get all possible expressions, including badly formatted ones (tryParse will filter those out)
 let expressions = buildExpressions();
-console.log(expressions);
+
+// evaluate all expressions (including badly formatted ones)
+evaluateExpressions(expressions)
+
+// let test = tryParse('10 + 1')
+// console.log(test)
+
+console.log("finished");
